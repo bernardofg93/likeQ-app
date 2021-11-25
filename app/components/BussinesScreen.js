355b0fc4d69863c6d2b5363a1fb82117365dfdb6
Steven <stevenvarela98@gmail.com>
2021-11-25 18:16:22 -0400

    :wrench: update, modal y consulta para turnos

--- app/components/BussinesScreen.js
+++ app/components/BussinesScreen.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react'
-import { Text, View, ScrollView, TouchableOpacity, Alert } from 'react-native';
+import { Text, View, ScrollView, TouchableOpacity, Alert, Modal, TextInput, Button, RefreshControl } from 'react-native';
 import bussinesScreenStyles from '../styles/bussinesScreenStyles';
 import { WaitingQueueComponent } from './WaitingQueueComponent';
 import { useDispatch, useSelector } from 'react-redux';
 import messaging from '@react-native-firebase/messaging'
+import firestore from '@react-native-firebase/firestore'
+import { status } from '../utils';
 
 export const BussinesScreen = () => {
-
+    const defaultFormState = {
+        name: '',
+        email: ''
+    }
+    const user = useSelector(state => state.user)
     const [titleButton, setTitleButton] = useState('Pedir Turno');
+    const [refreshing, setRefreshing] = useState(false)
+    const [visible, setVisible] = useState(false)
+    const [form, setForm] = useState(defaultFormState)
     const {currentTurn, waitingQueue} = useSelector(({waitingQueue,currentTurn}) => {
         return {
             waitingQueue,
@@ -30,22 +39,45 @@ export const BussinesScreen = () => {
 
     const getTurn = () => {
         if(titleButton === 'Pedir Turno'){
-            dispatch({
-                type: 'INCREASE_WAITING_QUEUE'
-            })
-            setMyTurn(waitingQueue+2)
+            setVisible(true)
             setTitleButton('Cancelar Turno');
         } else {
             showAlert();
         }
     }
+    const onSubmit = async () => {
+        try {
+            console.log('>>: form > ', form)
+            // dispatch({
+            //     type: 'SET_USER',
+            //     payload: form
+            // })
+        } catch (error) {
+            console.log('>>: error > ', error)
+        }
+    }
 
+    const onClose = () => {
+        setVisible(false)
+        setForm(defaultFormState)
+    }
     const cancelTurn = () => {
-        dispatch({
-            type: 'TO_DISCOUNT_A_WAITING_QUEUE'
-        })
-        setMyTurn(0)
-        setTitleButton('Pedir Turno');
+        if(form.email || user.email){
+            setRefreshing(true)
+            firestore().collection('turns')
+                .where('status', '==', status.ACTIVE)
+                .where('email', '==', form.email || user.email)
+                .update({
+                    status: status.INACTIVE
+                })
+                .then(() => {
+                    setMyTurn(0)
+                    setTitleButton('Pedir Turno');
+                    setRefreshing(false)
+                });
+            if(visible)
+                setVisible(false)
+        }
     }
 
     const showAlert = () => {
@@ -62,10 +94,42 @@ export const BussinesScreen = () => {
 
         )
     }
-
     return (
         <>
-            <ScrollView style={bussinesScreenStyles.scroll}>
+            <Modal
+                visible={visible}
+                animationType='slide'
+            >
+                <View>
+                    <Text>Ingrese su nombre</Text>
+                    <TextInput
+                        onChangeText={text => console.log('>>: text > ', text)}
+                    />
+                    <Text>Ingrese su email</Text>
+                    <TextInput
+                        onChangeText={text => console.log('>>: text > ', text)}
+                    />
+                    <View
+                        style={{flexDirection: 'row'}}
+                    >
+                        <Button
+                            onPress={() => onSubmit()}
+                            title='Reservar turno'
+                        />
+                        <Button
+                            onPress={() => onClose()}
+                            title='Cerrar'
+                        />
+                    </View>
+                </View>
+            </Modal>
+            <ScrollView
+                style={bussinesScreenStyles.scroll}
+                refreshControl={<RefreshControl
+                    refreshing={refreshing}
+                    // onRefresh={() => handleRefresh()}
+                />}
+            >
                 <View style={bussinesScreenStyles.content}>
                     <Text style={bussinesScreenStyles.titleScreen}>Este negocio tiene aproximadamente 20</Text>
                     <Text style={bussinesScreenStyles.titleScreen}>minutos de espera</Text>
