7cf44a5aec0c9ebab870804c80e484516bf8dd18
Steven <stevenvarela98@gmail.com>
2021-11-25 18:16:09 -0400

    :wrench: nuevop reducer para usuario en turno

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,7 +2,9 @@ import { combineReducers } from 'redux'
 import { TypedUseSelectorHook, useSelector } from "react-redux"
 import waitingQueue from './WaitingQueue'
 import currentTurn from './CurrentTurn'
+import user from './User'
 export const rootReducer =  combineReducers({
+    user,
     waitingQueue,
     currentTurn
 });
